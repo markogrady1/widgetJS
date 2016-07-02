10e15e5294037c96b6c86c33c74b1368877a828b
markogrady1 <markogrady.code@gmail.com>
2016-07-02 13:36:07 +0100

    refactor widgit.js

--- widgit.js
+++ widgit.js
@@ -1,32 +1,18 @@
-
 "use strict";
 
 var Widgit = (function() {
 
 	//ADD STYLES
-	
-	var style = document.currentScript.src;
-	var styles = document.getElementsByTagName("script");
-	var newPath = style.split("widgit.js")[0];
-	newPath = newPath + "css/widgit.css"
- 	var head = document.querySelectorAll("head")[0];
- 	var link = create("link");
- 	attr(link, "rel", "stylesheet");
- 	attr(link, "href", newPath);
- 	var link2 = create("link");
- 	attr(link2, "rel", "stylesheet");
- 	attr(link2, "href", "https://cdnjs.cloudflare.com/ajax/libs/octicons/3.5.0/octicons.css");
- 	appendElement(head, link, link2);
-
+	addStyle();
 
     function reposWidget(selector, username, amount) {
-        apiRequest(username, {"urlParam":"users", "targetParam":"repos"} , function(res) {
+        apiRequest(username, { "urlParam": "users", "targetParam": "repos" } , function(res) {
             var data = JSON.parse(res);
-            if(amount != null)
+            if( amount != null )
             	data = data.splice(0, amount);
             var key = ["name", "full_name", "language", "fork", "html_url"];
             var repoArr = [];
-            for(var i in data) {
+            for( var i in data ) {
                 repoArr.push({
                     name: data[i].name,
                     full_name: data[i].full_name,
@@ -43,10 +29,10 @@ var Widgit = (function() {
     }
 
 	function overviewWidget(selector, username) {
-		apiRequest(username, {"urlParam":"users", "targetParam":""}, function(res) {
+		apiRequest(username, { "urlParam": "users", "targetParam": "" }, function(res) {
 			var data = JSON.parse(res);
 			var overviewArr = [data.public_repos, data.public_gists, data.followers, data.avatar_url, data.repos_url, data.gists_url,  data.followers_url, data.html_url, data.login];
-			createType("overview", overviewArr, selector);
+			createType( "overview", overviewArr, selector );
 		});
 	}
 
@@ -56,11 +42,11 @@ var Widgit = (function() {
             + "/" + user.replace("/", "");
         var target = urlData.targetParam.length === 0
             ? ""
-            : "/" +urlData.targetParam.replace("/","") + "?sort=updated";
+            : "/" +urlData.targetParam.replace("/", "") + "?sort=updated";
         url = url + target;
 		var xobj = new XMLHttpRequest();
 	    xobj.overrideMimeType("application/json");
-	    xobj.open('GET',url, true); 
+	    xobj.open('GET', url, true); 
 	    xobj.onreadystatechange = function () {
 		    if (xobj.readyState == 4 && xobj.status == "200") {
 		    	callback(xobj.responseText);
@@ -83,19 +69,14 @@ var Widgit = (function() {
         var el = create("div");
         attr(el, "class", prependStr + "view-wrap");
         addTitle(el, arr);
-        el = createElem(el, "div",1);
+        el = createElem(el, "div", 1);
         var ul = create("ul");
         attr(ul, "class", prependStr + "ul");
-        ul =  typeof arr[0] == "object"
-            ? createRepoElem(ul, "li",3, arr,  true)
-            : createElem(ul, "li",3, arr,  true)
+		element(ul, arr);
         appendElement(el, ul);
         elem.appendChild(el)
-        var nxt =  typeof arr[0] == "object"
-            ? repo(arr, elem, selector)
-            : overview(arr, elem, selector);
+		view(elem, arr, selector);
     }
-
 
     function repo(arr, elem, selector) {
         addAvatar(".repo__view-wrap", arr);
@@ -106,6 +87,18 @@ var Widgit = (function() {
         addAvatar(".o__view-wrap", arr);
     }
 
+	function element(param, arr) {
+		param =  typeof arr[0] == "object"
+            ? createRepoElem(param, "li", 3, arr,  true)
+            : createElem(param, "li", 3, arr,  true)
+	}
+
+	function view(param, arr, selector) {
+		var e =  typeof arr[0] == "object"
+            ? repo(arr, param, selector)
+            : overview(arr, param, selector);
+	}
+
 	function addTitle(elem, arr) {
         var username;
         username = (typeof arr[8] !== "object") ? arr[8] : arr[0].username;
@@ -113,23 +106,6 @@ var Widgit = (function() {
 
         }
 		elem.innerHTML = "<span class=username>" + username + "</span>";
-	}
-
-	function addAvatar(element, data) {
-        var avatar;
-        var link;
-        if(typeof data[0] != "object") {
-            avatar = data[3];
-            link = data[7];
-        } else {
-            avatar = data[0].avatar_url;
-            link = data[0].owner_url;
-        }
-
-		var elem = document.querySelectorAll(element + " div")[0];
-		elem.innerHTML = "<a href=" + link + " target=__blank><img class=avatar src=" + avatar + "></a>";
-		attr(elem, "class", "header");
-        var title = (typeof data[0] !== "object") ? attr(elem, "class", "header"): attr(elem, "class", "header-repo");
 	}
 
 	function addData(option, data) {
@@ -172,15 +148,16 @@ var Widgit = (function() {
     }
 
 	function action(index, data) {
+		var indexPosition = data.length-2;
 		switch(index) {
 			case 1:
-			 	window.open(data[data.length-1] + "?tab=repositories");
+			 	window.open(data[indexPosition] + "?tab=repositories");
 			 	break;
 			case 2:
-			 	window.open(data[data.length-1].replace("//", "//gist."))
+			 	window.open(data[indexPosition].replace("//", "//gist."))
 			 	break;
 			case 3:
-			 	window.open(data[data.length-1] + "/followers");
+			 	window.open(data[indexPosition] + "/followers");
 			 	break;
 		}
 	}
@@ -212,9 +189,40 @@ var Widgit = (function() {
 			if ( elem.setAttribute )
 				elem.setAttribute(name,value);
 		}
-	return elem[name] || elem.getAttribute(name) || '';
+		return elem[name] || elem.getAttribute(name) || '';
 	}
 
+	function addStyle() {
+		var style = document.currentScript.src;
+		var styles = document.getElementsByTagName("script");
+		var newPath = style.split("widgit.js")[0];
+		newPath = newPath + "css/widgit.css"
+		var head = document.querySelectorAll("head")[0];
+		var link = create("link");
+		attr(link, "rel", "stylesheet");
+		attr(link, "href", newPath);
+		var link2 = create("link");
+		attr(link2, "rel", "stylesheet");
+		attr(link2, "href", "https://cdnjs.cloudflare.com/ajax/libs/octicons/3.5.0/octicons.css");
+		appendElement(head, link, link2);
+	}
+
+	function addAvatar(element, data) {
+        var avatar;
+        var link;
+        if(typeof data[0] != "object") {
+            avatar = data[3];
+            link = data[7];
+        } else {
+            avatar = data[0].avatar_url;
+            link = data[0].owner_url;
+        }
+		var elem = document.querySelectorAll(element + " div")[0];
+		elem.innerHTML = "<a href=" + link + " target=__blank><img class=avatar src=" + avatar + "></a>";
+		attr(elem, "class", "header");
+        var title = (typeof data[0] !== "object") ? attr(elem, "class", "header") : attr(elem, "class", "header-repo");
+	}
+	
 	return {
 		overView: overviewWidget,
         repos: reposWidget
